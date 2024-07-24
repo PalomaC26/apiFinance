91487bb937cf65d64e4b426c5aed4e7fcc8b319f
unknown <PalomaC26>
2024-07-24 10:43:48 -0300

    iniciando API 1

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
 //Importar a biblioteca mysql2 e criar a conexão com o banco de dados
 
-const mysql = require('mysql2') // importar o pacote mysql para conectar ao Banco De Dados
+const mysql = require('mysql2'); // importar o pacote mysql para conectar ao Banco De Dados
 
 mysql.createConnection({
-  host:process.env.DB_HDST, //Endereço do servidor de Banco De Dados
+  host:process.env.DB_HOST, //Endereço do servidor de Banco De Dados
  user:process.env.DB_USER,  //Nome de ususario para acessar o Banco de Dados
  password:process.env.DB_PASS, //senha do usuario paar acessar o Banco de dados
  database:process.env.DB_NAME //nome de Banco de Dados
 });
 
-db.connec((err) =>{
+db.connect((err) =>{
     if(err){
         console.log('Erro ao conectar ao Banco De Dados' , err); //Exibe a mensagem de erro
     return;
